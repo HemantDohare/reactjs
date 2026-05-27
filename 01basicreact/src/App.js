008717273a89f4b5be4b7a7e19fdf679a2ab4f01
HemantDohare <hemantdohare30@gamil.com>
2026-05-27 22:31:56 +0530

    firstpush

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Hello Hemant</h1>
+  );
+}
+
+export default App;
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+
+
+  //let counter = 5
+
+
+  const addValue = () => {
+    
+   // counter = counter + 1
+    setCounter(counter + 1)
+  }
+  const removeValue = () => {
+    
+    //counter = counter - 1
+    setCounter(counter - 1)
+
+  }
+
+  return (
+    <>
+     <h1>Counter App</h1>
+     <h2>counter value : {counter}</h2>
+
+     <button
+     onClick={addValue}>Add value</button>
+     <br />
+     <button
+     onClick={removeValue}
+     >Remove value {counter}</button>
+     <p> Footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
